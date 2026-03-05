e687c32c019cfb51cef108e5ccd9adbd5149f54e
Fábio Sanzzoni <168327984+fabiosanzzoni@users.noreply.github.com>
2026-03-05 11:55:52 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import flet as ft
+
+def main(page: ft.Page):
+
+    page.title = "Lista de Tarefas Infinity"
+    page.padding = 10
+
+    lista_tarefas = ft.Column()
+
+    campo_tarefa = ft.TextField(
+        label="Digite a tarefa: ",
+    )
+
+    def adicionar_tarefa(e):
+        if campo_tarefa.value.strip() == "":
+            campo_tarefa.value = "Escreva o nome da tarefa!"
+
+        nova_tarefa = ft.Row([
+            ft.Text(campo_tarefa.value),
+        ])
+
+        lista_tarefas.controls.append(nova_tarefa)
+
+        campo_tarefa.value = ""
+
+        page.update()
+
+    botao_adicionar = ft.Button(
+        "Adicionar Tarefa",
+        on_click=adicionar_tarefa
+    )
+
+    page.add(
+        ft.Row([campo_tarefa, botao_adicionar], alignment=ft.MainAxisAlignment.CENTER),
+        lista_tarefas
+    )
+
+ft.run(main)
